f6162e43582d8912223ad9eb65cde5770ba16b03
md amran hossain <amran.web.dev@gmail.com>
2021-10-03 17:39:22 +0600

    this is first commit

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use JWTAuth;
+use App\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    protected $user;
+
+    public function __construct()
+    {
+        $this->user = JWTAuth::parseToken()->authenticate();
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $student = $this->user->students()->get(["id", "student_id", "name", "details"])->toArray();
+        return $student;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            "name" => "required",
+            "details"=> "required",
+        ]);
+
+        $student = new Student();
+        $student->name = $request->name;
+        $student->details = $request->details;
+
+        if ($this->user->students->save($student)) {
+
+        return response()->json([
+            "status"=> true,
+            "user"=> $student
+        ]);
+        } else {
+            return response()->json([
+                "status"=> false,
+                "message"=> "Student Can not Save."
+            ], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Student $student)
+    {
+        $this->validate($request, [
+            "name" => "required",
+            "details"=> "required",
+        ]);
+
+        $student->name = $request->name;
+        $student->details = $request->details;
+
+        if ($this->user->students->save($student)) {
+
+        return response()->json([
+            "status"=> true,
+            "student"=> $student
+        ]);
+        } else {
+            return response()->json([
+                "status"=> false,
+                "message"=> "Student Can not Update."
+            ], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Student $student)
+    {
+        if ($student->delete()) {
+
+            return response()->json([
+                "status"=> true,
+                "student"=> $student
+            ]);
+            } else {
+                return response()->json([
+                    "status"=> false,
+                    "message"=> "Student Can not be delete."
+                ]);
+            }
+    }
+}
---
+++ database/migrations/2021_10_03_075012_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('student_id');
+            $table->string('name');
+            $table->string('details');
+            $table->timestamps();
+
+            $table->foreign('student_id')->references('id')->on('users')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
